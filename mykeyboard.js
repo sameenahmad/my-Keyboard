1ba8e1e3f00170545df2bf27e3cef6f8c9f2ac0b
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-03-26 20:57:34 +0530

    js-with audio files-not working

--- mykeyboard.js
+++ mykeyboard.js
@@ -4,45 +4,55 @@ window.addEventListener("keydown", function checkKeyPress(event) {
   switch (pushButton) {
     case "a":
       console.log("Hello, I am a");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('a').play();
       break;
     case "s":
       console.log("Hello, I am s");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('s').play();
       break;
     case "d":
       console.log("Hello, I am d");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('d').play();
       break;
     case "f":
       console.log("Hello, I am f");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('f').play();
       break;
     case "g":
       console.log("Hello, I am g");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('g').play();
       break;
     case "h":
       console.log("Hello, I am h");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('h').play();
       break;
     case "i":
       console.log("Hello, I am i");
-      checkBtn(pushButton);
+      //checkBtn(pushButton);
+      document.getElementById('i').play();
       break;
     case "j":
       console.log("Hello, I am j");
-      checkBtn(pushButton);
+     // checkBtn(pushButton);
+      document.getElementById('j').play();
       break;
     default:
-      console.log("Key not present");
+      alert("Key not present");
   }
 });
-function checkBtn(keyvalue) {
+
+/* function checkBtn(keyvalue) {
   for (i = 0; i < button.length; i++) {
     var btnText = button.innerHTML;
     if (btnText == keyValue) {
-      button.style.background = "pink";
+      button.style.background = "hotpink";
     }
   }
 }
+*/
