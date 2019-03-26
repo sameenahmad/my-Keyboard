d81b09f1eda82962e96dc09a6a1ee04df6e268b6
Abhishek Soni <abhisheksoni2720@gmail.com>
2019-03-26 21:18:21 +0530

    fix loop

--- mykeyboard.js
+++ mykeyboard.js
@@ -1,51 +1,35 @@
-var button = document.getElementsByClassName("button");
+var buttons = document.getElementsByClassName("button");
 
 window.addEventListener("keydown", function checkKeyPress(event) {
   var pushButton = event.key;
+  checkBtn(pushButton);
+
   switch (pushButton) {
     case "a":
-      console.log("Hello, I am a");
-      //checkBtn(pushButton);
       document.getElementById("a").play();
       break;
     case "s":
-      console.log("Hello, I am s");
-      //checkBtn(pushButton);
       document.getElementById("s").play();
       break;
     case "d":
-      console.log("Hello, I am d");
-      //checkBtn(pushButton);
       document.getElementById("d").play();
       break;
     case "f":
-      console.log("Hello, I am f");
-      //checkBtn(pushButton);
       document.getElementById("f").play();
       break;
     case "g":
-      console.log("Hello, I am g");
-      //checkBtn(pushButton);
       document.getElementById("g").play();
       break;
     case "h":
-      console.log("Hello, I am h");
-      //checkBtn(pushButton);
       document.getElementById("h").play();
       break;
     case "i":
-      console.log("Hello, I am i");
-      //checkBtn(pushButton);
       document.getElementById("i").play();
       break;
     case "j":
-      console.log("Hello, I am j");
-      // checkBtn(pushButton);
       document.getElementById("j").play();
       break;
     case "k":
-      console.log("Hello, I am k");
-      // checkBtn(pushButton);
       document.getElementById("k").play();
       break;
     default: {
@@ -55,12 +39,13 @@ window.addEventListener("keydown", function checkKeyPress(event) {
   }
 });
 
-/* function checkBtn(keyvalue) {
-  for (i = 0; i < button.length; i++) {
-    var btnText = button.innerHTML;
-    if (btnText == keyValue) {
-      button.style.background = "hotpink";
+function checkBtn(keyValue) {
+  for (i = 0; i < buttons.length; i++) {
+    var btnText = buttons[i].innerText;
+    btnText = btnText.toLowerCase();
+
+    if (btnText === keyValue) {
+      buttons[i].style.background = "hotpink";
     }
   }
 }
-*/
