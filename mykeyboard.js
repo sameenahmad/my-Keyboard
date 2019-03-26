5e2f1abb9f0c9f81ea669bac42fec576ffa97fae
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-03-26 20:04:31 +0530

    Added js logic- Not working

--- mykeyboard.js
+++ mykeyboard.js
@@ -1,12 +1,48 @@
-window.onload= main;
-
-function main()
-{
-var button= document.querySelector('.button');
-function myfunction(event)
-{var btnText=event.target.innerText;
-    console.log(btnText);
-
-}
-button.addeventlistener('click', myfunction) ;
+var button = document.getElementsByClassName("button");
+window.addEventListener("keydown", function checkKeyPress(event) {
+  var pushButton = event.key;
+  switch (pushButton) {
+    case "a":
+      console.log("Hello, I am a");
+      checkBtn(pushButton);
+      break;
+    case "s":
+      console.log("Hello, I am s");
+      checkBtn(pushButton);
+      break;
+    case "d":
+      console.log("Hello, I am d");
+      checkBtn(pushButton);
+      break;
+    case "f":
+      console.log("Hello, I am f");
+      checkBtn(pushButton);
+      break;
+    case "g":
+      console.log("Hello, I am g");
+      checkBtn(pushButton);
+      break;
+    case "h":
+      console.log("Hello, I am h");
+      checkBtn(pushButton);
+      break;
+    case "i":
+      console.log("Hello, I am i");
+      checkBtn(pushButton);
+      break;
+    case "j":
+      console.log("Hello, I am j");
+      checkBtn(pushButton);
+      break;
+    default:
+      console.log("Key not present");
+  }
+});
+function checkBtn(keyvalue) {
+  for (i = 0; i < button.length; i++) {
+    var btnText = button.innerHTML;
+    if (btnText == keyValue) {
+      button.style.background = "pink";
+    }
+  }
 }
