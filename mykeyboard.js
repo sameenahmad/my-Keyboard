3f76c12cbc9f07e6d20da533fbb0fa5d70006d26
Abhishek Soni <abhisheksoni2720@gmail.com>
2019-03-26 21:27:18 +0530

    refactor code

--- mykeyboard.js
+++ mykeyboard.js
@@ -3,40 +3,8 @@ let buttons = document.getElementsByClassName("button");
 window.addEventListener("keydown", function checkKeyPress(event) {
   let pushButton = event.key;
   checkBtn(pushButton);
-
-  switch (pushButton) {
-    case "a":
-      document.getElementById("a").play();
-      break;
-    case "s":
-      document.getElementById("s").play();
-      break;
-    case "d":
-      document.getElementById("d").play();
-      break;
-    case "f":
-      document.getElementById("f").play();
-      break;
-    case "g":
-      document.getElementById("g").play();
-      break;
-    case "h":
-      document.getElementById("h").play();
-      break;
-    case "i":
-      document.getElementById("i").play();
-      break;
-    case "j":
-      document.getElementById("j").play();
-      break;
-    case "k":
-      document.getElementById("k").play();
-      break;
-    default: {
-      // do nothing
-      return;
-    }
-  }
+  const curElem = document.getElementById(pushButton);
+  if (!curElem) curElem.play();
 });
 
 function checkBtn(keyValue) {
